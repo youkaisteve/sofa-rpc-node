e334ced90e57ed8846cf782eb167090f91c1711d
steve.k.you <steve.k.you@yzw.cn>
2021-10-04 15:27:28 +0800

    add debug code

--- example/client.js
+++ example/client.js
@@ -5,22 +5,22 @@ const { ZookeeperRegistry } = require('../').registry;
 const logger = console;
 
 const registry = new ZookeeperRegistry({
-  logger,
-  address: '127.0.0.1:2181',
+    logger,
+    address: '119.45.31.41:2181',
 });
 
 async function invoke() {
-  const client = new RpcClient({
-    logger,
-    registry,
-  });
-  const consumer = client.createConsumer({
-    interfaceName: 'com.nodejs.test.TestService',
-  });
-  await consumer.ready();
+    const client = new RpcClient({
+        logger,
+        registry,
+    });
+    const consumer = client.createConsumer({
+        interfaceName: 'com.nodejs.test.TestService',
+    });
+    await consumer.ready();
 
-  const result = await consumer.invoke('plus', [ 1, 2 ], { responseTimeout: 3000 });
-  console.log('1 + 2 = ' + result);
+    const result = await consumer.invoke('plus', [1, 2], { responseTimeout: 3000 });
+    console.log('1 + 2 = ' + result);
 }
 
 invoke().catch(console.error);
--- example/server.js
+++ example/server.js
@@ -5,25 +5,30 @@ const { ZookeeperRegistry } = require('../').registry;
 const logger = console;
 
 const registry = new ZookeeperRegistry({
-  logger,
-  address: '127.0.0.1:2181',
+    logger,
+    address: '119.45.31.41:2181',
 });
 
 const server = new RpcServer({
-  logger,
-  registry,
-  port: 12200,
+    logger,
+    registry,
+    port: 12200,
 });
 
-server.addService({
-  interfaceName: 'com.nodejs.test.TestService',
-}, {
-  async plus(a, b) {
-    return a + b;
-  },
-});
+server.addService(
+    {
+        interfaceName: 'com.nodejs.test.TestService',
+    },
+    {
+        async plus(a, b) {
+            return a + b;
+        },
+    }
+);
 
-server.start()
-  .then(() => {
+server.start().then(() => {
     server.publish();
-  });
+    setInterval(() => {
+        console.log(process.memoryUsage().heapUsed);
+    }, 50000);
+});
